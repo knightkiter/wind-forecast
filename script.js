15ff788dc83ee848527447a5c6fba3a583e3bb4d
Michael Lanser <kiteknight@gmail.com>
2026-08-14 20:11:13 -0400

    Rename weekly recommendation to Best Kiteboarding Forecast

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ async function loadForecast() {
 
         weekHTML += `
             <p>
-                <strong>Best Day This Week:</strong>
+                <strong>Best Kiteboarding Forecast::</strong>
                 ${bestDayName} ${weekBestTime} -
                 ${winds[weekBestIndex]} mph,
                 gust ${gusts[weekBestIndex]} mph,
